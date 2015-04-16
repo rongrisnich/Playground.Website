d7abed094894c38421695870472ff84c3ded9b4e
rongrisnich <ron.grisnich@trivident.com>
2015-04-16 17:17:20 +0200

    Initial commit

---
+++ Playground.Website/Controllers/ComponentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DD4T.Mvc.Controllers;
+using System.Web.Mvc;
+using DD4T.ContentModel;
+using DD4T.ContentModel.Exceptions;
+using DD4T.Factories;
+using System.Reflection;
+//using BuildingBlocks.DD4T.MarkupModels;
+using DD4T.Mvc.Html;
+using DD4T.ContentModel.Factories;
+
+namespace Playground.Website.Controllers
+{
+    public class ComponentController : TridionControllerBase
+    {
+        public ComponentController()
+        {
+            ComponentFactory = new ComponentFactory();
+        }
+        private IComponentFactory _componentFactory = null;
+        public override DD4T.ContentModel.Factories.IComponentFactory ComponentFactory
+        {
+            get
+            {
+                if (_componentFactory == null)
+                    _componentFactory = new ComponentFactory();
+                return _componentFactory;
+            }
+        }
+
+
+    }
+}
---
+++ Playground.Website/Controllers/PageController.cs
@@ -0,0 +1,77 @@
+﻿using DD4T.ContentModel.Factories;
+using DD4T.Factories;
+using DD4T.Mvc.Controllers;
+using DD4T.Mvc.Html;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Playground.Website.Controllers
+{
+    public class PageController : TridionControllerBase
+    {
+
+        private Regex reDefaultPage = new Regex(@".*/[^/\.]*(/?)$");
+
+        public PageController()
+        {
+            componentPresentationRenderer = new DefaultComponentPresentationRenderer();
+        }
+
+        private DD4T.Factories.PageFactory _pageFactory = null;
+        private string _defaultPageFileName = null;
+
+
+        public override DD4T.ContentModel.Factories.IPageFactory PageFactory
+        {
+            get
+            {
+                if (_pageFactory == null)
+                {
+                    _pageFactory = new PageFactory();
+                    _pageFactory.LinkFactory = new LinkFactory();
+                }
+                return _pageFactory;
+            }
+        }
+        public string DefaultPageFileName
+        {
+            get
+            {
+                if (_defaultPageFileName == null)
+                {
+                    _defaultPageFileName = ConfigurationManager.AppSettings["DD4T.DefaultPage"];
+                }
+                return _defaultPageFileName;
+            }
+        }
+
+        public override ActionResult Page(string pageId)
+        {
+            if (string.IsNullOrEmpty(pageId))
+            {
+                pageId = DefaultPageFileName;
+            }
+            else
+            {
+                if (reDefaultPage.IsMatch("/" + pageId))
+                {
+                    if (pageId.EndsWith("/"))
+                    {
+                        pageId += DefaultPageFileName;
+                    }
+                    else
+                    {
+                        pageId += "/" + DefaultPageFileName;
+                    }
+                }
+            }
+            return base.Page(pageId);
+        }
+
+    }
+}
